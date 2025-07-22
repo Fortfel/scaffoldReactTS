bf455b4e2141ef573b48364f1596e5d033d9526b
Marcin Dubiel <fortfel@gmail.com>
2025-07-22 22:08:08 +0200

    finishing setting up tailiwndcss/Vite/shadcn combo

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import legacy from '@vitejs/plugin-legacy'
 import VitePluginBrowserSync from 'vite-plugin-browser-sync'
@@ -12,7 +13,7 @@ export default defineConfig(({ mode }) => {
   // Use root path for development and repository name for production
   const base = mode === 'production' ? '/' : '/'
 
-  const isDebugMode: boolean = Boolean(environment.DEBUG ?? false)
+  // const isDebugMode: boolean = Boolean(environment.DEBUG ?? false)
   const serverPort: number = Number(environment.PORT ?? 3000)
   // const API_URL = environment.VITE_API_URL ?? 'http://localhost:3000'
 
@@ -20,6 +21,7 @@ export default defineConfig(({ mode }) => {
     base,
     plugins: [
       react(),
+      tailwindcss(),
       tsconfigPaths({
         root: './',
         projects: ['tsconfig.app.json'],
@@ -39,31 +41,31 @@ export default defineConfig(({ mode }) => {
       outDir: 'dist',
       emptyOutDir: true,
     },
-    server: {
-      ...(mode === 'development' && {
-        proxy: {
-          // Proxy API requests to the server during development
-          '/smartbrain-api': {
-            target: `http://localhost:${serverPort.toString()}`,
-            changeOrigin: true,
-            secure: false,
-            ws: true,
-            ...(isDebugMode && {
-              configure: (proxy, _options): void => {
-                proxy.on('error', (err, _req, _res) => {
-                  console.log('proxy error', err)
-                })
-                proxy.on('proxyReq', (_proxyReq, req, _res) => {
-                  console.log('Sending Request to the Target:', req.method, req.url)
-                })
-                proxy.on('proxyRes', (proxyRes, req, _res) => {
-                  console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
-                })
-              },
-            }),
-          },
-        },
-      }),
-    },
+    // server: {
+    //   ...(mode === 'development' && {
+    //     proxy: {
+    //       // Proxy API requests to the server during development
+    //       '/api': {
+    //         target: `http://localhost:${serverPort.toString()}`,
+    //         changeOrigin: true,
+    //         secure: false,
+    //         ws: true,
+    //         ...(isDebugMode && {
+    //           configure: (proxy, _options): void => {
+    //             proxy.on('error', (err, _req, _res) => {
+    //               console.log('proxy error', err)
+    //             })
+    //             proxy.on('proxyReq', (_proxyReq, req, _res) => {
+    //               console.log('Sending Request to the Target:', req.method, req.url)
+    //             })
+    //             proxy.on('proxyRes', (proxyRes, req, _res) => {
+    //               console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+    //             })
+    //           },
+    //         }),
+    //       },
+    //     },
+    //   }),
+    // },
   }
 })
